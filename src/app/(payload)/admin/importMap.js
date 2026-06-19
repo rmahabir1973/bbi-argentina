97cb30db329bb3fc4195e025e23a356e2cd53430
Ryan Mahabir <ryan@mahabir.pro>
2026-06-19 11:37:49 -0400

    fix: pre-populate importMap.js with S3ClientUploadHandler for admin route

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,1 +1,5 @@
-export const importMap = {}
+import { S3ClientUploadHandler as S3ClientUploadHandler_0 } from '@payloadcms/storage-s3/client'
+
+export const importMap = {
+  '@payloadcms/storage-s3/client#S3ClientUploadHandler': S3ClientUploadHandler_0,
+}
